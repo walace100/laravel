ac17059b3eb39e98dd656bb2812d2afd9fc4e484
Walace Paz <walacepaz11@gmail.com>
2020-05-22 00:32:11 -0300

    correção na aula de views

--- resources/views/pagina.blade.php
+++ resources/views/pagina.blade.php
@@ -13,7 +13,7 @@
 		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-		<script type="text/javascript" src="{{asset('js/app.js')}}"></script>
-		<!-- <script type="text/javascript" src="{{URL::to('js/app.js')}}"></script> -->
+		<script type="text/javascript" src="{{URL::to('js/app.js')}}"></script>
+		<!-- <script type="text/javascript" src="{{asset('js/app.js')}}"></script> -->
 	</body>
 </html>
